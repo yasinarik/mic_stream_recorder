8d1dc40482b587b10af6db6e6f210860f0a4ac63
yasinarik <yasin.ariky@gmail.com>
2025-06-11 00:18:54 +0300

    Refactor test files for improved readability and maintainability; add mock methods for recording functionality in MockMicStreamRecorderPlatform.

--- test/mic_stream_recorder_test.dart
+++ test/mic_stream_recorder_test.dart
@@ -7,13 +7,43 @@ import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 class MockMicStreamRecorderPlatform
     with MockPlatformInterfaceMixin
     implements MicStreamRecorderPlatform {
-
   @override
   Future<String?> getPlatformVersion() => Future.value('42');
+
+  @override
+  Future<void> startRecording() => Future.value();
+
+  @override
+  Future<String?> stopRecording() => Future.value('/path/to/recording.m4a');
+
+  @override
+  Future<void> playRecording([String? filePath]) => Future.value();
+
+  @override
+  Future<void> pausePlayback() => Future.value();
+
+  @override
+  Future<void> stopPlayback() => Future.value();
+
+  @override
+  Future<bool> isPlaying() => Future.value(false);
+
+  @override
+  Future<void> configureRecording({
+    double? sampleRate,
+    int? channels,
+    int? bufferSize,
+    int? audioQuality,
+  }) =>
+      Future.value();
+
+  @override
+  Stream<double> getAmplitudeStream() => Stream.fromIterable([0.0, 0.5, 1.0]);
 }
 
 void main() {
-  final MicStreamRecorderPlatform initialPlatform = MicStreamRecorderPlatform.instance;
+  final MicStreamRecorderPlatform initialPlatform =
+      MicStreamRecorderPlatform.instance;
 
   test('$MethodChannelMicStreamRecorder is the default instance', () {
     expect(initialPlatform, isInstanceOf<MethodChannelMicStreamRecorder>());
@@ -21,7 +51,8 @@ void main() {
 
   test('getPlatformVersion', () async {
     MicStreamRecorder micStreamRecorderPlugin = MicStreamRecorder();
-    MockMicStreamRecorderPlatform fakePlatform = MockMicStreamRecorderPlatform();
+    MockMicStreamRecorderPlatform fakePlatform =
+        MockMicStreamRecorderPlatform();
     MicStreamRecorderPlatform.instance = fakePlatform;
 
     expect(await micStreamRecorderPlugin.getPlatformVersion(), '42');
